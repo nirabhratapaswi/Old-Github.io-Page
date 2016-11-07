bc0fbe3411d481a4216cc96fec6084b1dcb3067c
nirabhratapaswi <nirabhratapaswi@gmail.com>
2016-11-08 01:58:38 +0530

    sidebar links added and keyboard shortcuts for page scoll added, and jquery-easing cdn used

--- javascript/index.js
+++ javascript/index.js
@@ -15,7 +15,11 @@ var toggle = true;
 	    });
 var key_shift = false,
 	key_o = false,
-	key_n = false;
+	key_n = false,
+	key_up = false,
+	key_down = false;
+
+var ref = 0;
 
 document.addEventListener("keyup", function(event) {
 	if(event.which == 16) {
@@ -26,6 +30,12 @@ document.addEventListener("keyup", function(event) {
 	}
 	if(event.which == 78) {
 		key_n = false;
+	}
+	if(event.which == 38) {
+		key_up = false;
+	}
+	if(event.which == 40) {
+		key_down = false;
 	}
 });
 
@@ -39,7 +49,66 @@ document.addEventListener("keydown", function(event) {
 	if(event.which == 78) {
 		key_n = true;
 	}
+	if(event.which == 38) {
+		key_up = true;
+	}
+	if(event.which == 40) {
+		key_down = true;
+	}
 	if(key_shift && key_n && key_o) {
 		$("#menu-toggle").click();
 	}
+	if(key_shift && key_n && key_up) {
+		if(ref>=3) {
+			ref=0;
+		}
+		else {
+			ref++;
+		}
+		if(ref==0) {
+			$( "#nav-ul li:nth-child(1) a" ).click();
+		}
+		else if(ref==1) {
+			$( "#nav-ul li:nth-child(2) a" ).click();
+		}
+		else if(ref==2) {
+			$( "#nav-ul li:nth-child(3) a" ).click();
+		}
+		else if(ref==3) {
+			$( "#nav-ul li:nth-child(4) a" ).click();
+		}
+	}
+	if(key_shift && key_n && key_down) {
+		if(ref<=0) {
+			ref=3;
+		}
+		else {
+			ref--;
+		}
+		if(ref==0) {
+			$( "#nav-ul li:nth-child(1) a" ).click();
+		}
+		else if(ref==1) {
+			$( "#nav-ul li:nth-child(2) a" ).click();
+		}
+		else if(ref==2) {
+			$( "#nav-ul li:nth-child(3) a" ).click();
+		}
+		else if(ref==3) {
+			$( "#nav-ul li:nth-child(4) a" ).click();
+		}
+	}
 });
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInCubic');//'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
